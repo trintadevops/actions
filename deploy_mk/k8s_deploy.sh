180cc08e64c626afa803af957a3c7966c2c5d67d
Trinta <trintoni@gmail.com>
2024-02-26 17:32:19 -0300

    insert sudo

--- deploy_mk/k8s_deploy.sh
+++ deploy_mk/k8s_deploy.sh
@@ -1,5 +1,5 @@
-apt-get install python3 -y
-apt-get install pip3 -y
+sudo apt-get install python3 -y
+sudo apt-get install pip3 -y
 pip3 install kubernetes
 pip3 install docker
 pip3 install kubeconfig
