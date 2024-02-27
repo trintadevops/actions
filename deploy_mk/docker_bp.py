3b6f1dbd8a105381da6eb8f28b827d20e509726a
Trinta <trintoni@gmail.com>
2024-02-27 09:27:24 -0300

    build_push

--- deploy_mk/docker_bp.py
+++ deploy_mk/docker_bp.py
@@ -7,10 +7,10 @@ PODNAME = os.environ.get('POD_NAME')
 WORKSPACE = os.environ.get('WORKSPACE')
 GITHUBWORKSPACE = os.environ.get('GITHUB_WORKSPACE')
 
-build = client.images.build(path = GITHUBWORKSPACE, tag = 'core.harbor.domain/library/' + PODNAME)
+# build = client.images.build(path = GITHUBWORKSPACE, tag = 'core.harbor.domain/library/' + PODNAME)
 
-client.login(registry='core.harbor.domain', username='admin', password='Harbor12345')
-push = client.images.push('core.harbor.domain/library/' + PODNAME)
+client.login(registry='registry.trintadevops.ddns.net', username='admin', password='Harbor12345')
+push = client.images.push('registry.trintadevops.ddns.net/library/' + PODNAME)
 print(push)
 
 
