b1337d45527fe5f0b6788a4e4a2c88369e20ada9
Trinta <trintoni@gmail.com>
2024-02-27 09:38:08 -0300

    update apt

--- deploy_mk/docker_build_push.sh
+++ deploy_mk/docker_build_push.sh
@@ -5,6 +5,7 @@ echo "HOSTNAME: ${HOSTNAME}"
 echo "WORKSPACE: ${GITHUB_ACTION_PATH}"
 echo "USERNAME: ${USER}"
 echo "#############################################"
+sudo apt-get update
 sudo apt-get install python3-pip -y
 pip3 install docker
 python3 ${GITHUB_ACTION_PATH}/docker_bp.py
