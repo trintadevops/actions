a8660d6e4ed35790e986d8f0cac9d19b686d81e7
Trinta <trintoni@gmail.com>
2024-02-27 14:21:32 -0300

    teste

--- deploy_mk/docker_bp.py
+++ deploy_mk/docker_bp.py
@@ -7,7 +7,7 @@ PODNAME = os.environ.get('POD_NAME')
 WORKSPACE = os.environ.get('WORKSPACE')
 GITHUBWORKSPACE = os.environ.get('GITHUB_WORKSPACE')
 
-build = client.images.build(path = GITHUBWORKSPACE, tag = '10.104.178.216/library/' + PODNAME)
+# build = client.images.build(path = GITHUBWORKSPACE, tag = '10.104.178.216/library/' + PODNAME)
 
 client.login(registry='10.104.178.216', username='admin', password='Harbor12345')
 push = client.images.push('10.104.178.216/library/' + PODNAME)
