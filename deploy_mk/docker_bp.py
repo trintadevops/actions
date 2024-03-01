6cf7892f81f861aed15f81eac5889adae8be2b39
Trinta <trintoni@gmail.com>
2024-03-01 19:03:32 -0300

    teste

--- deploy_mk/docker_bp.py
+++ deploy_mk/docker_bp.py
@@ -4,13 +4,13 @@ import os
 client = docker.from_env()
 
 PODNAME = os.environ.get('POD_NAME')
-WORKSPACE = os.environ.get('WORKSPACE')
-GITHUBWORKSPACE = os.environ.get('GITHUB_WORKSPACE')
+# WORKSPACE = os.environ.get('WORKSPACE')
+# GITHUBWORKSPACE = os.environ.get('GITHUB_WORKSPACE')
 
-# build = client.images.build(path = GITHUBWORKSPACE, tag = '10.104.178.216/library/' + PODNAME)
+# # build = client.images.build(path = GITHUBWORKSPACE, tag = '10.104.178.216/library/' + PODNAME)
 
-client.login(registry='10.104.178.216', username='admin', password='Harbor12345')
-push = client.images.push('10.104.178.216/library/' + PODNAME)
+client.login(registry='registry.trintadevops.ddns.net', username='admin', password='Harbor12345')
+push = client.images.push('registry.trintadevops.ddns.net/library/' + "quarkus-social")
 print(push)
 
 
