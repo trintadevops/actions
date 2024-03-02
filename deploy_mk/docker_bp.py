82dc7309a45f85f7780be8a14568159c4b502781
Trinta <trintoni@gmail.com>
2024-03-02 15:39:05 -0300

    teste

--- deploy_mk/docker_bp.py
+++ deploy_mk/docker_bp.py
@@ -7,10 +7,10 @@ PODNAME = os.environ.get('POD_NAME')
 # WORKSPACE = os.environ.get('WORKSPACE')
 # GITHUBWORKSPACE = os.environ.get('GITHUB_WORKSPACE')
 
-# # build = client.images.build(path = GITHUBWORKSPACE, tag = '10.104.178.216/library/' + PODNAME)
+# # build = client.images.build(path = GITHUBWORKSPACE, tag = 'harbor-portal.harbor.svc.cluster.local/library/' + PODNAME)
 
-client.login(registry='10.96.13.127', username='admin', password='Harbor12345')
-push = client.images.push('10.96.13.127/library/' + "quarkus-social")
+client.login(registry='harbor-core.harbor.svc.cluster.local', username='admin', password='Harbor12345')
+push = client.images.push('harbor-portal.harbor.svc.cluster.local/library/' + "quarkus-social")
 print(push)
 
 
