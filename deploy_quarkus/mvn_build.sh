6037c2c9cf7fded1d0d09deaa5bd7dd4ef3f2957
Trinta <trintoni@gmail.com>
2024-03-07 14:42:01 -0300

    teste

--- deploy_quarkus/mvn_build.sh
+++ deploy_quarkus/mvn_build.sh
@@ -1,5 +1,7 @@
+mkdir -p /home/runner/.docker
 sudo chmod 777 /home/runner/.docker
 sudo chown runner. /home/runner/.docker
+cp /home/runner/config.json /home/runner/.docker/
 sudo apt-get update
 sudo apt-get install python3 python3-pip maven -y
 pip3 install kubernetes
