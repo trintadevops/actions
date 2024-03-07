c75aaa0147dde82118c54f74e6c1159a4ede821e
Trinta <trintoni@gmail.com>
2024-03-07 13:58:20 -0300

    teste

--- deploy_quarkus/mvn_build.sh
+++ deploy_quarkus/mvn_build.sh
@@ -7,6 +7,7 @@ echo "############### INSTALACAO MAVEN WRAPER #################"
 mvn wrapper:wrapper
 echo "############### FIM DA INSTALACAO MAVEN WRAPER #################"
 echo "CAMINHO GITHUB_ACTION_PATH ${GITHUB_ACTION_PATH}"
+sudo chmod 777 /home/runner/.docker
 ./mvnw quarkus:build -DskipTests
 
 # python3 ${GITHUB_ACTION_PATH}/k8sdeploy.py
