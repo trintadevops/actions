70157fc16725bffb20a7184f1bbe77a75c710e7e
Trinta <trintoni@gmail.com>
2024-03-08 14:10:39 -0300

    teste

--- deploy_quarkus/mvn_build.sh
+++ deploy_quarkus/mvn_build.sh
@@ -6,11 +6,13 @@ sudo apt-get update
 sudo apt-get install python3 python3-pip maven -y
 pip3 install kubernetes
 pip3 install docker
-pip3 install kubeconfig
+# pip3 install kubeconfig
 echo "############### INSTALACAO MAVEN WRAPER #################"
 mvn wrapper:wrapper
 echo "############### FIM DA INSTALACAO MAVEN WRAPER #################"
 echo "CAMINHO GITHUB_ACTION_PATH ${GITHUB_ACTION_PATH}"
 ./mvnw quarkus:build -DskipTests
+echo "############### FIM DA INSTALACAO MAVEN WRAPER #################"
+docker image ls
 
 # python3 ${GITHUB_ACTION_PATH}/k8sdeploy.py
